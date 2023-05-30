e8e89ea21cf096e782adc0a7500af9329eab96fe
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-05-30 20:42:34 +0400

    Add project files.

---
+++ MVCTest/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCTest.Models;
+using System.Diagnostics;
+
+namespace MVCTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Action
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hello()
+        {
+            string name = "john";
+
+            ViewData["name"] = name;
+            ViewBag.Name = name;
+
+
+            TempData["name"] = name;
+
+            return View(model: name);
+        }
+    }
+}
